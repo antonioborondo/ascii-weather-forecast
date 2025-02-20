db5072952e3fd6fbb8bf712e4813a2c28dcddce5
Antonio Borondo <antonioborondo@outlook.com>
2025-02-20 16:49:20 +0100

    Receive arguments

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,13 @@
 #include <curl/curl.h>
 
-int main() {
+int main(int argc, char** argv) {
+  char url[256];
+  sprintf(url, "https://wttr.in/%s", argv[1]);
+
   const auto curl_handle{curl_easy_init()};
   if (curl_handle) {
-    curl_easy_setopt(curl_handle, CURLOPT_URL, "https://wttr.in?0AFQ");
+    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
     curl_easy_perform(curl_handle);
     curl_easy_cleanup(curl_handle);
   }
