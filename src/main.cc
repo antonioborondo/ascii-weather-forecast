120c26994e71c4b1b9ff150ac13a5671b360bdeb
Antonio Borondo <antonioborondo@outlook.com>
2025-02-21 12:12:29 +0100

    Add wttr.in wrapper

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,14 @@
-#include <curl/curl.h>
+#include <string>
+
+#include "wttrin_wrapper.h"
 
 int main(int argc, char** argv) {
-  char url[256];
-  sprintf(url, "https://wttr.in/%s", argv[1]);
-
-  const auto curl_handle{curl_easy_init()};
-  if (curl_handle) {
-    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
-    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-    curl_easy_perform(curl_handle);
-    curl_easy_cleanup(curl_handle);
+  std::string options;
+  if (argc > 1) {
+    options = argv[1];
   }
+
+  wttrin_wrapper::GetWeatherForecast(options);
 
   return 0;
 }
