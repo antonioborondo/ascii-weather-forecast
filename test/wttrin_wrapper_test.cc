78df9451019e3b6daf2a8f0d92f38a7a84ce268c
Antonio Borondo <antonioborondo@outlook.com>
2025-12-30 11:30:12 +0100

    Add new test for wttr.in wrapper

--- test/wttrin_wrapper_test.cc
+++ test/wttrin_wrapper_test.cc
@@ -2,20 +2,42 @@
 
 #include <memory>
 
-#include "http_client_factory.h"
+#include "http_client_exception.h"
 #include "mock_http_client.h"
+#include "wttrin_wrapper_exception.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 using namespace testing;
 
 TEST(WttrinWrapperTest, GetWeatherForecast) {
-  auto mock_http_client_factory = []() {
+  const auto weather_forecast{"Sunny"};
+  auto mock_http_client_factory = [&weather_forecast]() {
     auto mock_http_client{std::make_unique<MockHttpClient>()};
-    EXPECT_CALL(*mock_http_client, GetUrl(_)).WillOnce(Return("Sunny"));
+    EXPECT_CALL(*mock_http_client, GetUrl(_))
+        .WillOnce(Return(weather_forecast));
     return mock_http_client;
   };
 
   WttrinWrapper wttrin_wrapper{mock_http_client_factory};
-  ASSERT_EQ("Sunny", wttrin_wrapper.GetWeatherForecast(""));
+  const std::string wttrin_options{""};
+  ASSERT_EQ(weather_forecast,
+            wttrin_wrapper.GetWeatherForecast(wttrin_options));
+}
+
+TEST(WttrinWrapperTest, GetWeatherForecastThrowsException) {
+  auto mock_http_client_factory = []() {
+    auto mock_http_client{std::make_unique<MockHttpClient>()};
+    const std::string exception_message{""};
+    EXPECT_CALL(*mock_http_client, GetUrl(_))
+        .WillOnce(Throw(HttpClientException(exception_message)));
+    return mock_http_client;
+  };
+
+  WttrinWrapper wttrin_wrapper{mock_http_client_factory};
+
+  const std::string wttrin_options{""};
+  ASSERT_THROW(wttrin_wrapper.GetWeatherForecast(wttrin_options),
+               WttrinWrapperException);
 }
